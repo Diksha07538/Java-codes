6500722047ae794e6d1acc0169b83081f21f8cab
Diksha <dikshalokhande0704@gmail.com>
2025-12-21 12:05:28 +0530

    Added infinite loop

---
+++ g.java
@@ -0,0 +1,13 @@
+//WRITE AN INFINITE LOOP USING DO WHILE CONDITION 
+import java.util.*;
+public class g {
+    public static void infiniteloop(){
+        do{
+             System.out.println("This is an infinite loop inside a function");
+        }while(true);
+    }
+public static void main(String args [] ){
+        Scanner sc = new Scanner(System.in);
+        infiniteloop();
+    }
+}
