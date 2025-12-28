43cec0cac0ae8c5241922bc2fc051f82b9d03731
Diksha <dikshalokhande0704@gmail.com>
2025-12-28 22:19:33 +0530

    matrix

---
+++ Matrix.java
@@ -0,0 +1,25 @@
+public class Matrix {
+    public static void main(String[] args) {
+
+        int[][] grid = {
+            {4, 3, 2, -1},
+            {3, 2, 1, -1},
+            {1, 1, -1, -2},
+            {-1, -1, -2, -3}
+        };
+
+        int count = 0;
+
+        // Loop through matrix
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] < 0) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println("Number of negative numbers: " + count);
+    }
+}
+
