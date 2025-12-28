258268ad03279175ec30d0528832e483533b0a3d
Diksha07538 <dikshalokhande0704@gmail.com>
2025-12-28 23:01:42 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+  // 2.sUM OF NUMBERS:
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        System.out.println("The grade:");
+        Scanner scanner = new Scanner(System.in); 
+        System.out.println("Taking input1 from the user:");
+        int a = scanner.nextInt();
+         System.out.println("Taking input2 from the user:");
+        int b = scanner.nextInt();
+        int sum = a+b;
+        System.out.println("The sum of these numbers is: ");
+        System.out.print(sum);
+        }
+}
+ 
+
+
+
