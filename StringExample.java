9fda64b061f55b1c4918e1e666401aeda2d140a1
Diksha07538 <dikshalokhande0704@gmail.com>
2025-12-29 20:45:31 +0530

    Add files via upload
    String basics

---
+++ StringExample.java
@@ -0,0 +1,11 @@
+public class StringExample {
+    public static void main(String[] args) {
+
+        String name = "Diksha";
+        String course = "Cyber Security";
+
+        System.out.println("Name: " + name);
+        System.out.println("Course: " + course);
+    }
+}
+
