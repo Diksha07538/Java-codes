5904c0ee9c2b1ba779e68c04a10cc595ff170ff9
Diksha07538 <dikshalokhande0704@gmail.com>
2025-12-31 22:09:02 +0530

    Print Array

--- Main.java
+++ Main.java
@@ -1,19 +1,18 @@
-  // 2.sUM OF NUMBERS:
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
-        System.out.println("The grade:");
-        Scanner scanner = new Scanner(System.in); 
-        System.out.println("Taking input1 from the user:");
-        int a = scanner.nextInt();
-         System.out.println("Taking input2 from the user:");
-        int b = scanner.nextInt();
-        int sum = a+b;
-        System.out.println("The sum of these numbers is: ");
-        System.out.print(sum);
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
- 
-
-
-
