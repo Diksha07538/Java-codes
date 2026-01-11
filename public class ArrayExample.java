0d8236bf368c2117caf47b50e63f991131265362
Diksha07538 <dikshalokhande0704@gmail.com>
2026-01-11 12:13:15 +0530

    Add files via upload

---
+++ public class ArrayExample.java
@@ -0,0 +1,12 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+
+        // Declare and initialize an array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Print array elements using loop
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
